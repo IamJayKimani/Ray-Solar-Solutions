49a558ca51d273537745d29fb0c73ee76ce692ad
sydney-lab <sydneyosindi03@gmail.com>
2026-08-27 21:24:36 +0300

    Comment out AppRoutes component
    Comment out the entire AppRoutes component and its imports.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,44 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import PublicLayout from '../layouts/PublicLayout';
-import AdminLayout from '../layouts/AdminLayout';
-import Home from '../pages/public/Home';
-import Products from '../pages/public/Products';
-import ProductDetails from '../pages/public/ProductDetails';
-import ProviderDashboard from '../pages/provider/ProviderDashboard';
-import ManageProviderProducts from '../pages/provider/ManageProducts';
-import AddProduct from '../pages/provider/AddProduct';
-import EditProduct from '../pages/provider/EditProduct';
-import AdminDashboard from '../pages/admin/AdminDashboard';
-import ManageUsers from '../pages/admin/ManageUsers';
-import ManageProviders from '../pages/admin/ManageProviders';
-import ManageProducts from '../pages/admin/ManageProducts';
-import Login from '../pages/auth/Login';
-import Register from '../pages/auth/Register';
-import ForgotPassword from '../pages/auth/ForgotPassword';
+// import { Routes, Route } from 'react-router-dom';
+// import PublicLayout from '../layouts/PublicLayout';
+// import AdminLayout from '../layouts/AdminLayout';
+// import Home from '../pages/public/Home';
+// import Products from '../pages/public/Products';
+// import ProductDetails from '../pages/public/ProductDetails';
+// import ProviderDashboard from '../pages/provider/ProviderDashboard';
+// import ManageProviderProducts from '../pages/provider/ManageProducts';
+// import AddProduct from '../pages/provider/AddProduct';
+// import EditProduct from '../pages/provider/EditProduct';
+// import AdminDashboard from '../pages/admin/AdminDashboard';
+// import ManageUsers from '../pages/admin/ManageUsers';
+// import ManageProviders from '../pages/admin/ManageProviders';
+// import ManageProducts from '../pages/admin/ManageProducts';
+// import Login from '../pages/auth/Login';
+// import Register from '../pages/auth/Register';
+// import ForgotPassword from '../pages/auth/ForgotPassword';
 
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route element={<PublicLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-      </Route>
-      <Route path="/provider" element={<ProviderDashboard />} />
-      <Route path="/provider/products" element={<ManageProviderProducts />} />
-      <Route path="/provider/products/add" element={<AddProduct />} />
-      <Route path="/provider/products/edit/:id" element={<EditProduct />} />
-      <Route element={<AdminLayout />}>
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/users" element={<ManageUsers />} />
-        <Route path="/admin/providers" element={<ManageProviders />} />
-        <Route path="/admin/products" element={<ManageProducts />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-    </Routes>
-  );
-}
+// function AppRoutes() {
+//   return (
+//     <Routes>
+//       <Route element={<PublicLayout />}>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/products/:id" element={<ProductDetails />} />
+//       </Route>
+//       <Route path="/provider" element={<ProviderDashboard />} />
+//       <Route path="/provider/products" element={<ManageProviderProducts />} />
+//       <Route path="/provider/products/add" element={<AddProduct />} />
+//       <Route path="/provider/products/edit/:id" element={<EditProduct />} />
+//       <Route element={<AdminLayout />}>
+//         <Route path="/admin" element={<AdminDashboard />} />
+//         <Route path="/admin/users" element={<ManageUsers />} />
+//         <Route path="/admin/providers" element={<ManageProviders />} />
+//         <Route path="/admin/products" element={<ManageProducts />} />
+//       </Route>
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/forgot-password" element={<ForgotPassword />} />
+//     </Routes>
+//   );
+// }
 
-export default AppRoutes;
+// export default AppRoutes;
